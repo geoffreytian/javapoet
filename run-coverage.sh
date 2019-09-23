862d7b5df968135519fb61112c18622fcf8cc40c
Aman Hemani <aman546052@gmail.com>
2019-09-23 17:25:56 -0500

    Update run-coverage.sh

--- run-coverage.sh
+++ run-coverage.sh
@@ -1,4 +1,11 @@
-cd AmanGeofTemporaryFolder/javapoet/target/site/jacoco
-cp index.html test-coverage.html
-mv test-coverage.html ../../../../..
 
+cd AmanGeofTemporaryFolder/javapoet
+mvn test
+cd /target/site/jacoco
+cp index.html test-coverage.html
+mv test-coverage.html ../../..
+mv jacoco.xml ../../..
+mv jacoco-resources ../../..
+mv jacoco-sessions.html ../../..
+mv jacoco.csv ../../..
+mv com.squareup.javapoet ../../..
